31dee02fd4ea2b30a2a4603b5bd5e352914cb6a5
ivanvikvik <ivanvikvik@gmail.com>
2023-05-02 20:03:04 +0300

    Initial commit

---
+++ counter.py
@@ -0,0 +1,19 @@
+class Counter:
+    def __init__(self, count=0):
+        self.__count = count
+
+    def get_count(self):
+        return self.__count
+
+    def increment(self):
+        self.__count += 1
+
+    def decrement(self):
+        if self.__count != 0:
+            self.__count -= 1
+
+    def reset(self):
+        self.__count = 0
+
+    def __str__(self):
+        return "Counter: " + str(self.__count)
